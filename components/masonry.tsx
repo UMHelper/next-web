cec872b6dcfd1e87b615b61cd76743a20590bc50
Box <me@boxz.dev>
2023-09-15 03:37:50 +0800

    masonry component

---
+++ components/masonry.tsx
@@ -0,0 +1,71 @@
+// 1.col
+// 2.row
+// 3.add
+
+import React, {ReactElement, ReactNode, useState} from "react";
+
+export const MasonryRow=({children,className}:{children:ReactNode,className:any})=>{
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    )
+}
+
+
+export const MasonryCol=(
+    {
+        children,
+        className
+    }:{
+        children:ReactNode[],
+        className:any
+    })=>{
+    console.log(children)
+    return(
+        <div className='flex-col space-y-4'>
+            {children.map((child,index)=>{
+                return (
+                    <MasonryRow key={index} className={className}>
+                        {child}
+                    </MasonryRow>
+                )
+            })}
+        </div>
+    )
+}
+
+export const Masonry=(
+  {
+      children,
+      col,
+      className,
+  }:{
+      children:ReactNode[]
+      col:number,
+      className:any
+  })=>{
+    const colList:ReactNode[][]=[]
+
+    for (let i = 0; i < col; i++) {
+        colList.push([])
+    }
+
+    for (let i = 0; i < children.length; i+=3) {
+        for (let j = 0; j < col; j++) {
+            colList[j].push(children[i+j])
+        }
+    }
+
+    return(
+        <div className='flex flex-row space-x-2'>
+            {colList.map((col:ReactNode[],index:number)=>{
+                return (
+                    <MasonryCol key={index} className={className}>
+                        {col}
+                    </MasonryCol>
+                )
+            })}
+        </div>
+    )
+}
