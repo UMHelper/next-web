b3283a810273f870b536b6b61d96e1e237ef5248
Box <me@boxz.dev>
2023-09-17 21:07:33 +0800

    fake api

--- consant.js
+++ consant.js
@@ -1,1 +1,134 @@
 export const FUZZY_SEARCH_COURSE=JSON.parse('{"course_info": [{"New_code": "ACCT1000", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT100", "courseTitleEng": "PRINCIPLES OF FINANCIAL ACCOUNTING", "courseTitleChi": "\u8ca1\u52d9\u6703\u8a08\u539f\u7406", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT2000", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT210", "courseTitleEng": "MANAGEMENT ACCOUNTING I", "courseTitleChi": "\u7ba1\u7406\u6703\u8a08\u5b78 I", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT2001", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT211", "courseTitleEng": "INTERMEDIATE ACCOUNTING I", "courseTitleChi": "\u4e2d\u7d1a\u6703\u8a08\u5b78 I", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT2002", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "Principles of Managerial Accounting", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT2003", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-BBEL332", "courseTitleEng": "BUSINESS LAW", "courseTitleChi": "\u5546\u696d\u6cd5", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT2004", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "Intermediate Accounting", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3000", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT212", "courseTitleEng": "INTERMEDIATE ACCOUNTING II", "courseTitleChi": "\u4e2d\u7d1a\u6703\u8a08\u5b78 II", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3001", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT214", "courseTitleEng": "INTERNATIONAL FINANCIAL REPORTING STANDARDS AND THE GLOBAL ENVIRONMENT", "courseTitleChi": "\u570b\u969b\u8ca1\u52d9\u5831\u544a\u6e96\u5247\u8207\u74b0\u7403\u74b0\u5883", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3002", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT311", "courseTitleEng": "MANAGEMENT ACCOUNTING II", "courseTitleChi": "\u7ba1\u7406\u6703\u8a08\u5b78 II", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3003", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT312", "courseTitleEng": "INTERMEDIATE ACCOUNTING III", "courseTitleChi": "\u4e2d\u7d1a\u6703\u8a08\u5b78 III", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3004", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT313", "courseTitleEng": "ADVANCED FINANCIAL ACCOUNTING", "courseTitleChi": "\u9ad8\u7d1a\u8ca1\u52d9\u6703\u8a08\u5b78", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3005", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT314", "courseTitleEng": "ACCOUNTING INFORMATION SYSTEMS", "courseTitleChi": "\u6703\u8a08\u8cc7\u8a0a\u7cfb\u7d71", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3006", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT351", "courseTitleEng": "INTERNSHIP FOR ACCOUNTING FUNCTION I", "courseTitleChi": "\u6703\u8a08\u529f\u80fd\u5be6\u7fd2 I", "Credits": 1.5, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3007", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT352", "courseTitleEng": "INTERNSHIP FOR ACCOUNTING FUNCTION II", "courseTitleChi": "\u6703\u8a08\u529f\u80fd\u5be6\u7fd2 II", "Credits": 1.5, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3008", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT424", "courseTitleEng": "PUBLIC SECTOR ACCOUNTING", "courseTitleChi": "\u516c\u5171\u6703\u8a08", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT3009", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "Financial Statement Analysis", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT4000", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT332", "courseTitleEng": "AUDITING I", "courseTitleChi": "\u5be9\u8a08\u5b78 I", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4001", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT420", "courseTitleEng": "ACCOUNTING THEORY", "courseTitleChi": "\u6703\u8a08\u5b78\u7406\u8ad6", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4002", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "Special Topics in Accounting", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4003", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "Design and Analysis of Cost Control Systems", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT4004", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT427", "courseTitleEng": "FINANCIAL ACCOUNTING IN HOSPITALITY AND GAMING", "courseTitleChi": "\u6b3e\u5ba2\u670d\u52d9\u53ca\u535a\u5f69\u696d\u8ca1\u52d9\u6703\u8a08", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4005", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT428", "courseTitleEng": "MANAGERIAL ACCOUNTING IN HOSPITALITY AND GAMING", "courseTitleChi": "\u6b3e\u5ba2\u670d\u52d9\u53ca\u535a\u5f69\u696d\u7ba1\u7406\u6703\u8a08", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4006", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT432", "courseTitleEng": "AUDITING II", "courseTitleChi": "\u5be9\u8a08\u5b78 II", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4007", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT433", "courseTitleEng": "CORPORATE GOVERNANCE", "courseTitleChi": "\u4f01\u696d\u7ba1\u6cbb", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4008", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT434", "courseTitleEng": "ACCOUNTING MANAGEMENT CONTROL SYSTEM", "courseTitleChi": "\u7ba1\u7406\u6703\u8a08\u7cfb\u7d71", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4009", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "China Tax", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4010", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT442", "courseTitleEng": "TAXATION IN MACAO AND HONG KONG", "courseTitleChi": "\u6fb3\u9580\u53ca\u9999\u6e2f\u7a05\u52d9", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT4011", "Offering_Unit": "FBA", "Old_code": "FBA-AIM-ACCT443", "courseTitleEng": "INTERNATIONAL TAXATION", "courseTitleChi": "\u570b\u969b\u7a05\u52d9", "Credits": 3.0, "Medium_of_Instruction": "English", "Offering_Department": "AIM"}, {"New_code": "ACCT7010", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7012", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FUNDAMENTALS OF ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7013", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FINANCIAL REPORTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7014", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "BUSINESS LAW", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7015", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "MANAGEMENT ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7016", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "PRINCIPLES OF ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7017", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FINANCIAL MANAGEMENT", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7018", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "ACCOUNTING FOR DECISION MAKING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7050", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "ADVANCED FINANCIAL REPORTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7051", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CORPORATE GOVERNANCE AND SOCIAL RESPONSIBILITY", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7052", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "PERFORMANCE MANAGEMENT AND CONTROL SYSTEMS", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7053", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FINANCIAL RISK MANAGEMENT", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7054", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CONTEMPORARY ISSUES IN ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7055", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "AUDITING AND ASSURANCE", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7056", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "TAXATION", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7057", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CHINA TAXATION", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English and Chinese", "Offering_Department": ""}, {"New_code": "ACCT7058", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "GOVERNMENTAL ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7059", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "HOSPITALITY AND GAMING ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7060", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FINANCIAL STATEMENT ANALYSIS FOR DECISION MAKING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7070", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "ACCOUNTING RESEARCH METHODS", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT7300", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "FINANCIAL STATEMENT ANALYSIS", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "Chinese and/or English", "Offering_Department": ""}, {"New_code": "ACCT7301", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CORPORATE FINANCIAL STRATEGY", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "Chinese and/or English", "Offering_Department": ""}, {"New_code": "ACCT7350", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CORPORATE GOVERNANCE STRATEGY", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "Chinese and/or English", "Offering_Department": ""}, {"New_code": "ACCT7351", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "INTERNATIONAL FINANCIAL MANAGEMENT STRATEGY", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "Chinese and/or English", "Offering_Department": ""}, {"New_code": "ACCT8011", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "ACCOUNTING RESEARCH METHODS AND SEMINAR", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}, {"New_code": "ACCT8012", "Offering_Unit": "FBA", "Old_code": "", "courseTitleEng": "CONTEMPORARY RESEARCH IN ACCOUNTING", "courseTitleChi": "", "Credits": 0.0, "Medium_of_Instruction": "English", "Offering_Department": ""}], "prof_info": []}')
+export const COURSE_INFO=JSON.parse('{\n' +
+  '    "course_info": {\n' +
+  '        "New_code": "ACCT1000",\n' +
+  '        "Offering_Unit": "FBA",\n' +
+  '        "Old_code": "FBA-AIM-ACCT100",\n' +
+  '        "courseTitleEng": "PRINCIPLES OF FINANCIAL ACCOUNTING",\n' +
+  '        "courseTitleChi": "財務會計原理",\n' +
+  '        "Credits": 3,\n' +
+  '        "Medium_of_Instruction": "English",\n' +
+  '        "Offering_Department": "AIM",\n' +
+  '        "courseDescription": "",\n' +
+  '        "Intended_Learning_Outcomes": ""\n' +
+  '    },\n' +
+  '    "prof_info": [\n' +
+  '        {\n' +
+  '            "name": "SAM IENG",\n' +
+  '            "result": 2.1190660760428193,\n' +
+  '            "grade": 2.05607235142119,\n' +
+  '            "attendance": 2.0575581395348848,\n' +
+  '            "hard": 1.8724160206718359,\n' +
+  '            "reward": 2.369250645994832,\n' +
+  '            "num": 172,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": true,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "CHAI LAI PING",\n' +
+  '            "result": 2.5712344720496887,\n' +
+  '            "grade": 2.6467391304347823,\n' +
+  '            "attendance": 2.496739130434783,\n' +
+  '            "hard": 2.1619565217391306,\n' +
+  '            "reward": 2.9195652173913045,\n' +
+  '            "num": 46,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "TCHIANG VAN KAM",\n' +
+  '            "result": 3.3321877551020402,\n' +
+  '            "grade": 3.6617238095238096,\n' +
+  '            "attendance": 2.4763333333333333,\n' +
+  '            "hard": 3.106038095238096,\n' +
+  '            "reward": 3.7585238095238096,\n' +
+  '            "num": 60,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": true,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "CHEN CAN",\n' +
+  '            "result": 2.8582417582417583,\n' +
+  '            "grade": 3.730769230769231,\n' +
+  '            "attendance": 1.2153846153846155,\n' +
+  '            "hard": 2.930769230769231,\n' +
+  '            "reward": 4.046153846153847,\n' +
+  '            "num": 13,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "CHAN WENG HANG",\n' +
+  '            "result": 3.7102255639097748,\n' +
+  '            "grade": 4.52842105263158,\n' +
+  '            "attendance": 2.513684210526316,\n' +
+  '            "hard": 3.7578947368421054,\n' +
+  '            "reward": 3.7947368421052636,\n' +
+  '            "num": 19,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": true,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "ZHANG XU",\n' +
+  '            "result": 3.5701984126984123,\n' +
+  '            "grade": 3.89,\n' +
+  '            "attendance": 2.89,\n' +
+  '            "hard": 3.11,\n' +
+  '            "reward": 3.89,\n' +
+  '            "num": 3,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "PUN SAU TAK",\n' +
+  '            "result": 1.3834674714956408,\n' +
+  '            "grade": 1.2255086071987487,\n' +
+  '            "attendance": 1.4549295774647886,\n' +
+  '            "hard": 1.3250391236306727,\n' +
+  '            "reward": 1.2109546165884195,\n' +
+  '            "num": 142,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "TERESA CHU",\n' +
+  '            "result": 1.9432900432900435,\n' +
+  '            "grade": 1.7515151515151515,\n' +
+  '            "attendance": 1.8606060606060608,\n' +
+  '            "hard": 1.603030303030303,\n' +
+  '            "reward": 2.309090909090908,\n' +
+  '            "num": 33,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        },\n' +
+  '        {\n' +
+  '            "name": "CARLOS NORONHA",\n' +
+  '            "result": 2.7232142857142856,\n' +
+  '            "grade": 2.5250000000000004,\n' +
+  '            "attendance": 3.2375,\n' +
+  '            "hard": 3.2625,\n' +
+  '            "reward": 3.5375,\n' +
+  '            "num": 8,\n' +
+  '            "offer_info": {\n' +
+  '                "is_offer": false,\n' +
+  '                "schedules": []\n' +
+  '            }\n' +
+  '        }\n' +
+  '    ]\n' +
+  '}')
