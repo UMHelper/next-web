ba4e12949201aa7ab2cffc18d35a163f3eec4ea1
Box <me@boxz.dev>
2023-09-19 19:57:57 +0800

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,20 +3,43 @@ import Link from "next/link";
 
 const Footer = () => {
     return(
-        <div className='bg-gray-500/10'>
-            <div className='max-w-screen-xl mx-auto p-4'>
-                <div className="flex flex-wrap items-center justify-start">
-                    <Cat size={24} strokeWidth={2} color='rgb(14 165 233)'/>
-                    <Link href="/" className="flex items-center mr-10">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-sky-500 to-indigo-600 bg-clip-text text-transparent">What2Reg @UM</span>
+        <div className='bg-gray-300/10'>
+            <div className='max-w-screen-xl mx-auto p-4 space-y-4 mb-4'>
+                <div>
+                    <div className="flex flex-wrap items-center justify-start">
+                        <Cat size={24} strokeWidth={2} color='rgb(14 165 233)'/>
+                        <Link href="/" className="flex items-center mr-10">
+                            <span className="self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-sky-500 to-indigo-600 bg-clip-text text-transparent">What2Reg @UM</span>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className='flex space-x-4 underline underline-offset-1 font-semibold text-sm'>
+                    <Link href='/'>
+                        GitHub
+                    </Link>
+                    <Link href='/'>
+                        Terms of Service
+                    </Link>
+                    <Link href='/'>
+                        Community Guidelines
+                    </Link>
+                    <Link href='/'>
+                        Feedback 反饋
+                    </Link>
+                    <Link href='/'>
+                        Sitemap
                     </Link>
                 </div>
-                <div className="flex flex-wrap items-center justify-start">
-                    <Link href="/" className="flex items-center mr-10">
-                        <span className="self-center text-base font-semibold whitespace-nowrap bg-gradient-to-r from-sky-500 to-indigo-600 bg-clip-text text-transparent">Built by UMHelper</span>
-                    </Link>
+                <div className='text-gray-500 text-xs'>
+                    Designed and built by the <Link className='text-black font-semibold underline underline-offset-1' href='/'>UMHelper</Link> team with the help of <Link className='text-black font-semibold underline underline-offset-1' href='/'>our contributors</Link>.
+                </div>
+                <div className='text-gray-500 text-xs'>
+                    Latest update <Link className='text-black font-semibold underline underline-offset-1' href='/'>2023-08-27T17:38:44Z</Link> licensed under <Link className='text-black font-semibold underline underline-offset-1' href='/'>GNU General Public License v3.0</Link> .
                 </div>
             </div>
+
+
         </div>
     )
 }
