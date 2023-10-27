36bb77221fec26c2cfb84e1ac14ad69e17917cd9
Box <me@boxz.dev>
2023-10-27 19:50:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <body className={cn(inter.className )}>
           <div className='min-h-screen'>
             <Navbar/>
