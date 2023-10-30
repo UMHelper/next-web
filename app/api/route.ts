c04032d65ac00eb3a9216f787a32e49541cca06f
Box <me@boxz.dev>
2023-10-31 01:05:56 +0800

    modified:   app/api/route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,5 @@
-import {connectToDatabase} from "@/lib/database";
 import {NextResponse} from "next/server";
 
 export async function GET(){
-    await connectToDatabase()
-
     return new NextResponse(JSON.stringify({code:1}))
 }
