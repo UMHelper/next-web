8b44ceca3568bbdcde53423d757b6eeffbb2ee10
Box <me@boxz.dev>
2023-11-09 23:44:39 +0800

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function NotFounfPage() {
+  // get path
+  const pathname = usePathname()
+  return(
+    <div className="w-full h-screen flex justify-center items-center flex-col space-y-8">
+      <div className="text-9xl font-black racking-widest bg-gradient-to-r from-teal-400 via-violet-400 to-blue-500 bg-clip-text text-transparent">
+        Oops :(
+      </div>
+      <div className="text-sm text-gray-400">
+        The page <span className="text-gray-800">{pathname}</span> is not found!
+      </div>
+      <div>
+        <Link href="/" className="text-blue-500 hover:underline">
+          Go back to Home
+        </Link>
+      </div>
+    </div>
+  )
+}
