b47d58ca744144db9b2bfa2f14e8cc7751c4a89e
Box <me@boxz.dev>
2023-11-12 03:00:07 +0800

    offered info of course card in search card

--- components/course_card.tsx
+++ components/course_card.tsx
@@ -8,12 +8,25 @@ const CourseCard=({data}:{data:any})=>{
         console.log(data.New_code,'click')
         router.push('/course/'+data.New_code)
     }
+    console.log(data)
     return(
         <Link href={'/course/'+data.New_code}>
             <Card className='hover:cursor-pointer hover:shadow-lg mx-auto'>
                 <CardHeader className='pb-2 flex-row flex justify-between align-middle'>
-                    <div>
-                        <CardTitle className='text-base'>{data.New_code}</CardTitle>
+                    <div className=" space-y-1">
+                        <CardTitle className='flex space-x-4'>
+
+                                <div className="text-xl">
+                                {data.New_code}
+                                </div>
+                                {
+                                    data.Is_Offered===1 ?
+                                        <div className='text-white text-xs rounded-3xl bg-gradient-to-r from-green-600 to-green-600 h-fit py-0.5 px-2 shadow'> Offered</div>
+                                        :
+                                        <div className='text-white text-xs rounded-3xl bg-gradient-to-r from-neutral-700 to-stone-900 h-fit py-0.5 px-2 shadow'> Not Offered</div>
+                                }
+                            
+                        </CardTitle>
                         <CardTitle className='text-base'>{data.courseTitleEng}</CardTitle>
                         <CardDescription>{data.courseTitleChi}</CardDescription>
                     </div>
