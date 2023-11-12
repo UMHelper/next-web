8bca283a11cd6f31cdb37039b03a43fdf469188a
Box <me@boxz.dev>
2023-11-12 13:42:14 +0800

    feat: sorting of course list in catalog page

--- app/catalog/[...departments]/page.tsx
+++ app/catalog/[...departments]/page.tsx
@@ -3,17 +3,16 @@ import supabase from '@/lib/database/database';
 
 const fetchCourseList=async(departments:string[])=>{
     if (departments.length===1){
-        const {data,error}=await supabase.from('course_noporf')
+        const {data,error}:{data:any,error:any}=await supabase.from('course_noporf')
                         .select('')
                         .eq('Offering_Unit',departments[0].toUpperCase())
-        console.log(data)
-        return data
+        return data.sort((a:any, b:any) => a.New_code.localeCompare(b.New_code))
     }
-    const {data,error}=await supabase.from('course_noporf')
+    const {data,error}:{data:any,error:any}=await supabase.from('course_noporf')
                         .select('')
                         .eq('Offering_Unit',departments[0].toUpperCase())
                         .eq('Offering_Department',departments[1].toUpperCase())
-    return data
+    return data.sort((a:any, b:any) => a.New_code.localeCompare(b.New_code))
     
 }
 const CatalogPage=async ({params:{departments}}:{params:{departments:string[]}})=>{
