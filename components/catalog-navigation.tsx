df705db26a408a54a6d9770e84a9e5c74280a69b
Box <me@boxz.dev>
2023-11-12 14:26:00 +0800

    fix: show status of catalog navigation

--- components/catalog-navigation.tsx
+++ components/catalog-navigation.tsx
@@ -8,11 +8,11 @@ const CatalogNavigation = () => {
     const currentFaculty = pathname.split('/')[2]
     return (
         <div className="flex flex-col md:flex-row md:space-x-2 space-y-2 md:space-y-0">
-            {faculty.map((faculty, index) => {
+            {faculty.map((fac, index) => {
                 return (
-                    <div key={index} className={faculty===pathname?"text-blue-700 px-1 rounded":
+                    <div key={index} className={fac===currentFaculty?"text-blue-700 px-1 rounded bg-gray-200":
                     "text-gray-900 hover:bg-gray-100 hover:text-blue-500 px-1 rounded"}>
-                        <Link href={`/catalog/${faculty}`}>{faculty}</Link>
+                        <Link href={`/catalog/${fac}`}>{fac}</Link>
                     </div>
                 )
             })}
