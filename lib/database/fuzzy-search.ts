70e456cb83c7312e383f1e4c3d6ad9dd0eb48b89
Box <me@boxz.dev>
2023-11-13 15:53:57 +0800

    fix: wrong course list in instruce search page

--- lib/database/fuzzy-search.ts
+++ lib/database/fuzzy-search.ts
@@ -13,17 +13,19 @@ export const fuzzySearch = async (keyword:string,type:string) => {
         return code_data.concat(title_data).sort((a:any, b:any) => a.New_code.localeCompare(b.New_code))
     }
     else{
+        let res:any=[]
         const {data:prof_list,error:prof_error}:{data:any,error:any}=await supabase.from('prof_info').select('name').ilike('name', `%${keyword.replaceAll("%20"," ").replaceAll('$', '/')}%`)
         const data= await Promise.all(
             prof_list.map(async ({name}:{name:string})=>{
-                const { data, error } = await supabase.rpc('get_course_list_by_prof', {prof:`%${keyword.replaceAll("%20"," ").replaceAll('$', '/')}%`})
-                return {
+                const { data, error } = await supabase.rpc('get_course_list_by_prof', {prof:name})
+                res.push ({
                     prof_name:name,
                     course_list:data
-                }
+                })
+                console.log(name,data,error)
             }
         ))
-        return data
+        return res
     }
     return []
 }
