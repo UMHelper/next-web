2d67cebee8866eb6dd550a9ef9d63fbfefdfafc2
Box <me@boxz.dev>
2023-11-16 00:02:10 +0800

    no header or footer in sign-in/sign-up page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,17 +2,12 @@
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
 import {AlignJustify} from "lucide-react";
 import {menuList as menu} from "@/lib/consant";
 import {usePathname} from "next/navigation";
 import Link from "next/link";
-import TimetableCart from "@/components/timetable-cart";
-import NavbarAvatar from "@/components/navbar-avatar";
 
 const MobileSidebar=()=>{
     const pathname=usePathname()
@@ -38,10 +33,6 @@ const MobileSidebar=()=>{
                                 </Link>
                             )
                         })}
-                        {/* <div className="flex flex-row space-x-2 px-1">
-                            <TimetableCart />
-                            <NavbarAvatar />
-                        </div> */}
                     </div>
                     
                 </SheetContent>
