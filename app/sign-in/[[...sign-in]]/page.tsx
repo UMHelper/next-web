3ecfb148d0c72f832876afa3e0d1fdf994467d10
Box <me@boxz.dev>
2023-11-16 14:20:10 +0800

    sign-in/up style update

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <SignIn />
+        <div className="w-full h-screen flex justify-center items-center">
+            <SignIn />
+        </div>
     );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-            <SignUp />
+            <div className="w-full h-screen flex justify-center items-center">
+                <SignUp />
+            </div>
         );
 }
