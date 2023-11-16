5ba23bf53be64ed969a5726081c78ff488e7278d
Box <me@boxz.dev>
2023-11-16 13:21:24 +0800

    rm avatar and cart in mobile navbar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,15 +4,17 @@ import {
     SheetContent,
     SheetTrigger,
 } from "@/components/ui/sheet"
-import {AlignJustify} from "lucide-react";
-import {menuList as menu} from "@/lib/consant";
-import {usePathname} from "next/navigation";
+import { AlignJustify } from "lucide-react";
+import { menuList as menu } from "@/lib/consant";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import TimetableCart from "@/components/timetable-cart";
+import NavbarAvatar from "@/components/navbar-avatar";
 
-const MobileSidebar=()=>{
-    const pathname=usePathname()
-    const menuList=menu
-    return(
+const MobileSidebar = () => {
+    const pathname = usePathname()
+    const menuList = menu
+    return (
         <div className='md:hidden w-full h-full flex justify-center items-center'>
             <Sheet>
                 <SheetTrigger>
@@ -22,19 +24,21 @@ const MobileSidebar=()=>{
                 </SheetTrigger>
                 <SheetContent>
                     <div className="font-bold flex flex-col p-4 mt-4 space-y-4">
-                        {menuList.map((menu:MenuItem)=>{
+                        {menuList.map((menu: MenuItem) => {
                             return (
                                 <Link href={menu.href}
-                                      className={menu.href===pathname?"text-blue-700 px-1 py-2 rounded":
-                                          "text-gray-900 hover:bg-gray-100 hover:text-blue-500 px-1 rounded py-2"}
-                                      key={menu.href}
+                                    className={menu.href === pathname ? "text-blue-700 px-1 py-2 rounded" :
+                                        "text-gray-900 hover:bg-gray-100 hover:text-blue-500 px-1 rounded py-2"}
+                                    key={menu.href}
                                 >
                                     {menu.name}
                                 </Link>
                             )
                         })}
+                    <NavbarAvatar />
+                    <TimetableCart />
                     </div>
-                    
+
                 </SheetContent>
             </Sheet>
         </div>
