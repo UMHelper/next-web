0e38ffd3594e103fd60ceaad340791e267d2af8e
Box <me@boxz.dev>
2023-11-19 00:17:16 +0800

    pwa

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,44 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#2563EB',
     icons: [
       {
-        src: '/favicon.png',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/icon/72.jpg',
+        sizes: '72x72',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/96.jpg',
+        sizes: '96x96',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/128.jpg',
+        sizes: '128x128',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/144.jpg',
+        sizes: '144x144',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/152.jpg',
+        sizes: '152x152',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/192.jpg',
+        sizes: '192x192',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/384.jpg',
+        sizes: '384x384',
+        type: 'image/jpg',
+      },
+      {
+        src: '/icon/512.jpg',
+        sizes: '512x512',
+        type: 'image/jpg',
       },
     ],
   }
