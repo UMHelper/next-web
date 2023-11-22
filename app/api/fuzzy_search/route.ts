7771c5e97b7a7e85db3e7fbacf185ad9903b9627
Box <me@boxz.dev>
2023-11-22 13:11:39 +0800

    fuzzy search api

--- app/api/fuzzy_search/route.ts
+++ app/api/fuzzy_search/route.ts
@@ -1,6 +1,16 @@
-import {NextResponse} from "next/server";
-import {FUZZY_SEARCH_COURSE} from "@/consant";
+import { NextRequest, NextResponse } from "next/server";
+import { FUZZY_SEARCH_COURSE } from "@/consant";
+import { fuzzySearch } from "@/lib/database/get-fuzzy-search";
 
-export async function GET(){
-    return new NextResponse(JSON.stringify(FUZZY_SEARCH_COURSE))
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url)
+    const type = searchParams.get('type')?.toLowerCase() || ""
+    const keyword = searchParams.get('keyword')?.toUpperCase() || ""
+    if (keyword === "" || type === "") return new NextResponse(JSON.stringify({ error: "keyword or type is empty" }))
+    if (type === "course" || type == "instructor") {
+        console.log(type, keyword)
+        const data = await fuzzySearch(keyword, type)
+        return new NextResponse(JSON.stringify(data))
+    }
+    return new NextResponse(JSON.stringify({ error: "type is not valid" }))
 }
