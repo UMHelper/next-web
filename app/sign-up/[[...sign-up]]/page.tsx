69448606783be4a89a83530f016bc6bcbe865da4
Box <me@boxz.dev>
2023-11-27 00:16:21 +0800

    modified:   app/sign-up/[[...sign-up]]/page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,11 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-            <div className="w-full h-screen flex justify-center items-center">
+            <div className="w-full h-screen flex justify-center items-center flex-col space-y-1">
+                <div className=" p-2 bg-red-500 rounded text-white text-center text-xs">
+                    <p>Our email verification system is blocked by University of Macau,</p> 
+                    <p>please use your personal email to sign up.</p>
+                </div>
                 <SignUp />
             </div>
         );
