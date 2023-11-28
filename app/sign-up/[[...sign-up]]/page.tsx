375d9f2d423b88301b7f27d747f95ebb6c41a6c9
UMHelper <140886735+umhelper-bot@users.noreply.github.com>
2023-11-28 22:10:56 +0800

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,12 +2,12 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-            <div className="w-full h-screen flex justify-center items-center flex-col space-y-1">
-                <div className=" p-2 bg-red-500 rounded text-white text-center text-xs">
-                    <p>Our email verification system is blocked by University of Macau,</p> 
-                    <p>please use your personal email to sign up.</p>
-                </div>
+            <div className="py-6 w-full h-screen flex justify-center items-center flex-col space-y-1">
                 <SignUp />
+                <div className="p-2 bg-red-500 rounded text-white text-center text-xs">
+                    <p>Our email verification system is blocked by University of Macau,</p> 
+                    <p>please use another email to sign up.</p>
+                </div>
             </div>
         );
 }
