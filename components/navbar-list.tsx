ba88d0998e1e1d2a07bf7c29fcdf1e5cd269c043
Box <me@boxz.dev>
2023-12-29 22:22:21 +0800

    icon link

--- components/navbar-list.tsx
+++ components/navbar-list.tsx
@@ -10,8 +10,8 @@ export default function NavbarList() {
 
     return (
         <div className="flex flex-wrap items-center justify-start ">
-                <Cat size={24} strokeWidth={2} color='rgb(14 165 233)' className="me-2"/>
                 <Link href="/" className="flex items-center mr-10">
+                    <Cat size={24} strokeWidth={2} color='rgb(14 165 233)' className="me-2"/>
                     <span className="hidden md:flex self-center text-lg md:text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-sky-500 to-indigo-600 bg-clip-text text-transparent">What2Reg @UM</span>
                 </Link>
             <div className="hidden w-full md:block md:w-auto">
