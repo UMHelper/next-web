4dd889eb601432cb2d277363b88c2df4ad67c4b4
Box <me@boxz.dev>
2024-01-01 20:00:57 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 
                     <meta name='theme-color' content='#2563EB' />
                     <meta name='apple-mobile-web-app-status-bar-style' content='#2563EB' />
-                    <meta name='viewport' content='width=device-width, initial-scale=1 maximum-scale=1' />
+                    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
                     <link rel="manifest" href="/manifest.webmanifest" />
                     <link rel="icon" href="/favicon.png" sizes="any" />
                     <link
