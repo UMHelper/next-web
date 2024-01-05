44134d883d33d5aae9aa3dc34b24a252701d088c
Box <me@boxz.dev>
2024-01-05 10:47:20 +0800

    update

--- lib/database/get-comment-list.ts
+++ lib/database/get-comment-list.ts
@@ -2,7 +2,7 @@ import supabase from '@/lib/database/database';
 
 export const getCommentList = async (course_id: string,prof:string) => {
     const { data, error } = await supabase.rpc('get_comment_list', {course_code:course_id,prof:prof.replaceAll("%20"," ").replaceAll('$', '/')})
-    return data.reverse()
+    return data.reverse().filter((comment:any)=>comment.hidden!==1) as any[]
 }
 
 export const getCommentNumber = async (course_id: string,prof:string) => {
