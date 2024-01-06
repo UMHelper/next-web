093eda47ae3824c1bb50d9b3d13040b69edb438c
Box <me@boxz.dev>
2024-01-07 04:26:36 +0800

    review pagination hydration fix

--- components/review-pagination.tsx
+++ components/review-pagination.tsx
@@ -1,7 +1,7 @@
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 
 export const ReviewPagination =
-    ({
+    async ({
         page_num,
         total_page,
         code,
@@ -12,19 +12,22 @@ export const ReviewPagination =
         code: string,
         prof: string
     }) => {
-        console.log(page_num, total_page, code, prof)
         return (
                 <Pagination>
                     <PaginationContent>
                         {
                             page_num > 1 ? (
-                                <PaginationItem>
                                     <PaginationPrevious href={`/reviews/${code}/${prof}/${page_num - 1}`} />
-                                </PaginationItem>
                             ) : null
                         }
                         {
-                            page_num > 2 ? (
+                            page_num > 3 ? (
+                                    <PaginationLink size='xs' href={`/reviews/${code}/${prof}`}>1</PaginationLink>
+                            ) : null
+
+                        }
+                        {
+                            page_num > 3 ? (
                                 <PaginationItem>
                                     <PaginationEllipsis />
                                 </PaginationItem>
@@ -34,26 +37,20 @@ export const ReviewPagination =
 
                         {
                             page_num - 1 > 0 ? (
-                                <PaginationItem>
                                     <PaginationLink size='xs' href={`/reviews/${code}/${prof}/${page_num - 1}`}>{page_num - 1}</PaginationLink>
-                                </PaginationItem>
                             ) : null
                         }
 
-                        <PaginationItem>
-                            <PaginationLink size='xs' href={`/reviews/${code}/${prof}/${page_num}`}>{page_num}</PaginationLink>
-                        </PaginationItem>
+                            <PaginationLink isActive size='xs' href={`/reviews/${code}/${prof}/${page_num}`}>{page_num}</PaginationLink>
 
                         {
                             page_num + 1 <= total_page ? (
-                                <PaginationItem>
                                     <PaginationLink size='xs' href={`/reviews/${code}/${prof}/${page_num + 1}`}>{page_num + 1}</PaginationLink>
-                                </PaginationItem>
                             ) : null
                         }
 
                         {
-                            total_page - page_num > 2 ? (
+                            total_page - page_num > 3 ? (
                                 <PaginationItem>
                                     <PaginationEllipsis />
                                 </PaginationItem>
@@ -61,10 +58,14 @@ export const ReviewPagination =
 
                         }
                         {
+                            total_page - page_num > 3 ? (
+                                    <PaginationLink size='xs' href={`/reviews/${code}/${prof}/${total_page}`}>{total_page}</PaginationLink>
+                            ) : null
+
+                        }
+                        {
                             page_num < total_page ? (
-                                <PaginationItem>
                                     <PaginationNext href={`/reviews/${code}/${prof}/${page_num + 1}`} />
-                                </PaginationItem>
                             ) : null
                         }
                     </PaginationContent>
