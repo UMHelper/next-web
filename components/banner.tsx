21116117e2a87ae2dc570794c8dba374422f2a12
Box <me@boxz.dev>
2025-12-09 13:51:10 +0800

    update

--- components/banner.tsx
+++ components/banner.tsx
@@ -17,10 +17,10 @@ export const Banner = () => {
                 rotationInterval={6000}
                 /> */}
             <div>
-                本網站與澳門大學不隸屬、關聯、授權、認可或以任何方式正式關聯。
+                <span className="whitespace-nowrap bg-gradient-to-r from-sky-600 to-indigo-600 bg-clip-text text-transparent">選咩課</span>與澳門大學不隸屬、關聯、授權、認可或以任何方式正式關聯。
             </div>
             <div className="text-center whitespace-pre-line break-words">
-                如果您經由其他網站或者應用程式訪問本網站，請注意您的個人信息安全。UMHelper 團隊與本網站並不對其他網站或者應用程式的安全性與信息準確性負責。
+                如果您經由其他網站或者應用程式訪問本網站，請注意您的個人信息安全。UMHelper 團隊與<span className="whitespace-nowrap bg-gradient-to-r from-sky-600 to-indigo-600 bg-clip-text text-transparent">選咩課</span>並不對其他網站或者應用程式的安全性與信息準確性負責。
             </div>
             {/* <div>
                 This website is not affiliated, associated, authorized, endorsed by, or in any way officially connected with the University of Macau.
