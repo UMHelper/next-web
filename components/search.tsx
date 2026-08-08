11848115a72c7fa204c41e8272a80c82cde93b89
Box <me@boxz.dev>
2026-08-08 16:52:25 +0800

    update

--- components/search.tsx
+++ components/search.tsx
@@ -186,7 +186,7 @@ export default function SearchComp() {
                             <div className='max-w-sm'>Search by course codes/titles, or name of instructors (partial search supported)</div>
                             <div>鍵入部分課程代碼/名稱或講師姓名</div><br></br>
 
-                            <div className=''>{process.env.NEXT_PUBLIC_CURRENT_YEAR}/{Number(process.env.NEXT_PUBLIC_CURRENT_YEAR)+1} AC Sem {process.env.NEXT_PUBLIC_CURRENT_SEM}</div>
+                            <div className=''>{process.env.NEXT_PUBLIC_CURRENT_YEAR}/{Number(process.env.NEXT_PUBLIC_CURRENT_YEAR)+1} AY Sem {process.env.NEXT_PUBLIC_CURRENT_SEM}</div>
                             <div className='italic'>Data Source: reg.um.edu.mo</div>
                             <div className='italic'>Last updated on: {process.env.NEXT_PUBLIC_DATABASE_LAST_UPDATE}</div>
                             
