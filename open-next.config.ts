4f16407d8f99c9d8995bd02e42099a60a7d32a0c
Box <me@boxz.dev>
2026-09-19 12:22:04 +0800

    feat: configure d1 tag cache for opennext

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
+  tagCache: d1NextTagCache,
 });
